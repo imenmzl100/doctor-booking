45dab00f5e5efd16b312d18c4798c6da4cfe7a90
hindomlz120 <hindomlz120@gmail.com>
2026-06-01 19:16:40 +0100

    remove doctor field from edit

--- resources/views/appointments/edit.blade.php
+++ resources/views/appointments/edit.blade.php
@@ -2,7 +2,6 @@
 <html>
 <head>
     <title>Modifier Rendez-vous</title>
-
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
@@ -17,85 +16,29 @@
         @method('PUT')
 
         <div class="mb-3">
-
-            <label>Médecin</label>
-
-            <select name="doctor_id" class="form-control">
-
-                @foreach($doctors as $doctor)
-
-                <option value="{{ $doctor->id }}"
-                    {{ $appointment->doctor_id == $doctor->id ? 'selected' : '' }}>
-
-                    {{ $doctor->name }}
-
-                </option>
-
-                @endforeach
-
-            </select>
-
-        </div>
-
-        <div class="mb-3">
-
             <label>Nom du patient</label>
-
-            <input
-                type="text"
-                name="patient_name"
-                class="form-control"
-                value="{{ $appointment->patient_name }}">
-
+            <input type="text" name="patient_name" class="form-control" value="{{ $appointment->patient_name }}">
         </div>
 
         <div class="mb-3">
-
             <label>Téléphone</label>
-
-            <input
-                type="text"
-                name="patient_phone"
-                class="form-control"
-                value="{{ $appointment->patient_phone }}">
-
+            <input type="text" name="patient_phone" class="form-control" value="{{ $appointment->patient_phone }}">
         </div>
 
         <div class="mb-3">
-
             <label>Date</label>
-
-            <input
-                type="date"
-                name="appointment_date"
-                class="form-control"
-                value="{{ $appointment->appointment_date }}">
-
+            <input type="date" name="appointment_date" class="form-control" value="{{ $appointment->appointment_date }}">
         </div>
 
         <div class="mb-3">
-
             <label>Heure</label>
-
-            <input
-                type="time"
-                name="appointment_time"
-                class="form-control"
-                value="{{ $appointment->appointment_time }}">
-
+            <input type="time" name="appointment_time" class="form-control" value="{{ $appointment->appointment_time }}">
         </div>
 
-        <button type="submit" class="btn btn-primary">
-            Modifier
-        </button>
-
-        <a href="{{ route('appointments.index') }}"
-           class="btn btn-secondary">
-            Retour
-        </a>
+        <button type="submit" class="btn btn-primary">Modifier</button>
+        <a href="{{ route('appointments.index') }}" class="btn btn-secondary">Retour</a>
 
     </form>
-
 </div>
 
 </body>
